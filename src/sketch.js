a3bea1bd205edd5c2ab5d166d411697357780ea4
Luis De Anda <andiyasha16@gmail.com>
2018-10-04 00:46:56 -0500

    Added Cell object

--- src/sketch.js
+++ src/sketch.js
@@ -1,9 +1,22 @@
+let cells = [];
+let scale = 10;
+let cellsPerRow;
+
 function setup() {
-    createCanvas(640, 480);
+    createCanvas(600, 600);
+
+    cellsPerRow = floor(width / scale);
+    for (let i = 0; i < cellsPerRow; i++) {
+        for (let j = 0; j < cellsPerRow; j++) {
+            var cell = new Cell(i * scale, j * scale, scale, false);
+            cells.push(cell);
+        }
+    }
 }
 
 function draw() {
-    for (let i = 0; i < 12; i++) {
-        ellipse(50 + i * 75, 50 + i * 25, 50, 50);
+    for (let cell of cells) {
+        cell.update();
+        cell.show();
     }
 }
